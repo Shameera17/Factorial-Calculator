7d280642beb1d2058a36118ccd867857fa9a2382
kaveenhyacinth <alwiskaveen@gmail.com>
2019-03-29 23:26:19 +0530

    Modify main.c, Add exit or continue feature, Fix the duplicating bug in fact

--- main.c
+++ main.c
@@ -4,21 +4,36 @@
 int main()
 {
     int x, i;
-    long long fact=1;
+    char conf;
+    long long fact=1;   // to output a big value
 
-    printf("Enter a number to calculate the factorial: ");
-    scanf("%d",&x);
-    printf("\n\n%d! = ", x);
+    while(1) {
+        printf("\nEnter a number to calculate the factorial: ");
+        scanf("%d",&x);
+        printf("\n\n%d! = ", x);
 
-    for(i=1; i<=x; i++)
-    {
-        fact*=i;
-        printf("%d ", i);
-
-        if(i<x)
+        for(i=1; i<=x; i++)
         {
-            printf("x ");
+            fact*=i;
+            printf("%d ", i);
+
+            if(i<x)
+            {
+                printf("x ");
+            }
         }
+
+        printf("\n\n%d! = %lld\n", x, fact);
+
+        printf("\nDo you want to exit [y/n]: ");
+        scanf("%s", &conf);
+
+        fact = 1;   // to avoid duplicating
+
+        if(conf == 'Y' || conf == 'y')
+            break;
+        else
+            continue;
     }
 
     return 0;
